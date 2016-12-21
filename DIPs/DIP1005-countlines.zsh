3a1bca0c634d80b4b396d6436a06887f1bcb12e0
Andrei Alexandrescu <andrei@erdani.com>
2016-12-21 09:59:07 -0500

    Change the way numbers are calculated for the section Workaround: Are Local Imports Good Enough?

--- DIPs/DIP1005-countlines.zsh
+++ DIPs/DIP1005-countlines.zsh
@@ -12,15 +12,19 @@ rm -f /tmp/test.log
 
 # Process all std files
 echo "|File|Imports (unittest)|Imports (compile)|Imports (top)"
-echo "|--|--|--|--|"
+echo "|---|---|---|---|"
 (
     cd $D
-    for f in std/**/*.d; do
+    for f in std/*.d std/{algorithm,container,digest,experimental,net,range,regex}/**/*.d; do
         echo "import $f;" | sed -e 's|/|.|g' -e 's/\.d;/;/' -e 's/\.package//' >/tmp/test.d
         echo -n "|$f"
-        LINES1=$(dmd -o- -c -v $f 2>>/tmp/test.log | grep '^import ' | wc -l)
-        LINES2=$(dmd -o- -c -v -unittest $f 2>>/tmp/test.log | grep '^import ' | wc -l)
-        LINES3=$(dmd -o- -c -v /tmp/test.d 2>>/tmp/test.log | grep '^import ' | wc -l)
-        echo "|$[LINES2-1]|$[LINES1-1]|$[LINES3-1]|"
+        LINES1=$(dmd -o- -c -v -unittest $f 2>>/tmp/test.log | \
+            grep '^import  *std\.' | wc -l)
+        LINES2=$(dmd -o- -c -v $f 2>>/tmp/test.log | \
+            grep '^import  *std\.' | wc -l)
+        LINES3=$(dmd -o- -c -v /tmp/test.d 2>>/tmp/test.log | \
+            grep '^import  *std\.' | wc -l)
+        # Subtract 1 from LINES3 to discount the imported file itself
+        echo "|$LINES1|$LINES2|$[LINES3-1]|"
     done | sort -t'|' --key=3 -nr
 )
